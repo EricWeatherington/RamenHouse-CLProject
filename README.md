047eec53eba19cfd2ed809cc6b4d79ea10f2aa75
Eric Weatherington <ericweatherington@gmail.com>
2022-11-17 13:59:33 -0500

    final commit before turning in

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # RamenHouse-CLProject
 Code Louisville WebDev1 Project - Mock website for restaurant Ramen House
+
+I am currently enrolled in Code Louisville's WebDev course 1, and for completion of this course I was instructed to create a website with a mobile first approach. 
+
+For my first project in WebDev, I decided to create a website for a local restaurant that I have close relations with.  I thought doing so would help me with my motivation and ultimately be more fun.  Easy to say that I did have a lot of fun with this one!
+
+The requirements for this project are to be visual appealing, be posted to github, and include a few of the features learned over the past few months with Code Louisville.  
+
+The features that I decided to include are:
+
+        - Responsive Menu with CSS/Javascript
+        - Hover Transitions within my Navbar
+        - Hide/Show Div for my hamburger menu in Mobile view
+        - Run PageSpeed Insights from google.com get a score of 60 or better
+
+
+Also this restaurant is really good, so check that out too if you have a chance!
+
+Thanks for checking out my project!
--- app.js
+++ app.js
@@ -23,45 +23,21 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
+
+ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
+
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace("active", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  
 }
-
-// about slideshow
-
-//var aboutSlideIndex = 1;
-//showDivs(aboutSlideIndex);
-
-//function plusDivs(n) {
-  //showDivs(aboutSlideIndex += n);
-//}
-
-//function showDivs(n) {
-  //var i;
-  //var x = document.getElementsByClassName("aboutSlides");
-  //if (n > x.length) {aboutSlideIndex = 1}
-  //if (n < 1) {aboutSlideIndex = x.length} ;
-  //for (i = 0; i < x.length; i++) {
-    
- // }
-  //x[aboutSlideIndex-1].style.display = "block";
-//}
